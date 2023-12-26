1921d589398f5080d0c432994239d56561f28b1c
bernard <bernard@pinochet.fr>
2023-12-26 21:55:02 +0100

    Carte: Petite correction référence R5 et R8 sur le schéma Création du programme de test de la carte: OK

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # duobot
-Robot à deux roues
+
+## Introduction
+
+Ce projet est un n-ième clone du célèbre "self balancing robot" que l'on peut voir un peut partout sur Internet.
+
+Voici un lien vers le projet original: https://electricdiylab.com/diy-self-balancing-robot/
+
+Pour ce projet, nous conservons les fondamentaux de l'original (moteur, drivers, accéléromètre, ...).
+
+Nous utiliserons les matériaux et les outils à notre à notre disposition. Les dimensions du robot seront également les nôtres.
+
+Et la carte sera développée dans le but d'être utilisée pour d'autres applications similaires et pour permettre la mise au point d'autres projets à base de moteurs pas-à-pas.
+
+## Construction du robot
+
+
+## Conception de la carte
+
+### Schéma
+
+Presque un copier-coller du modèle d'origine.
+
+Les différences principales sont les suivantes:
+- Pull up sur le Enable plutôt que des pull-down sur le Step de chaque driver;
+- Branchement des M0, M1 et M2 sur l'arduino (pour permettre le réglage de la résolution);
+- Possibilité de brancher soit un drv4988 (comme l'original), soit un drv8825 (broche VDD ou Fault que l'on peut connecter grâce à des résistances 0 Ohms);
+- Ajout de condensateurs de découplage (comme préconisé dans la datasheet des drivers);
+- Sortie des broches non utilisées sur un connecteur d'extension.
+
+### Routage du PCB
+
+Complètement revu pour une carte moins dense (un peu plus grande) de 100mm x 70mm.
+Les connecteurs seront des borniers plutôt que des JST comme sur le modèle. Ca permet un peu plus de souplesse lors des essais...
+
+### Vue en 3D
+
+## Mise en service
+
+### Réglage du courant MAX
+
+On tourne le potentiomètre des drivers jusqu'à obtenir 1V. Cela correspond à 250mA environ. Ce réglage sera à afiner en fonction des performances et de la limite admissible par le moteur...
+
+### Programme de test de la carte
+
+Le programme permet de tester la carte.
+
+- Il affiche les valeurs lues sur l'accéléromètre
+- il permet d'actionner les moteurs à l'aide des commandes.
+
---
+++ soft/duobot_test/duobot_test.ino
@@ -0,0 +1,199 @@
+#include <GY521.h>
+
+#define PIN_DIR_DTE     (3)
+#define PIN_STEP_DTE    (4)
+#define PIN_EN_DTE      (5)
+#define PIN_DIR_GHE     (6)
+#define PIN_STEP_GHE    (7)
+#define PIN_EN_GHE      (8)
+
+#define PIN_M2          (9)
+#define PIN_M1          (10)
+#define PIN_M0          (11)
+
+GY521 sensor(0x68);
+
+void init_gy521()
+{
+  Wire.begin();
+
+  delay(100);
+  while (sensor.wakeup() == false)
+  {
+    Serial.println("\tImpossible de se connecteur au GY521");
+    delay(1000);
+  }
+  sensor.setAccelSensitivity(0);  //  2g
+  sensor.setGyroSensitivity(0);   //  250 degrees/s
+
+  sensor.setThrottle();
+  
+  sensor.axe = -0.1128369;
+  sensor.aye = 0.0038721;
+  sensor.aze = -1.0322095;
+  sensor.gxe = 8.0835113;
+  sensor.gye = -1.0030534;
+  sensor.gze = -0.5611451;  
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("BOOT Duobot.");
+
+  pinMode(PIN_DIR_DTE,OUTPUT);
+  digitalWrite(PIN_DIR_DTE,LOW);
+  pinMode(PIN_STEP_DTE,OUTPUT);
+  digitalWrite(PIN_STEP_DTE,LOW);
+  pinMode(PIN_EN_DTE,OUTPUT);
+  digitalWrite(PIN_EN_DTE,HIGH);
+
+  pinMode(PIN_DIR_GHE,OUTPUT);
+  digitalWrite(PIN_DIR_GHE,LOW);
+  pinMode(PIN_STEP_GHE,OUTPUT);
+  digitalWrite(PIN_STEP_GHE,LOW);
+  pinMode(PIN_EN_GHE,OUTPUT);
+  digitalWrite(PIN_EN_GHE,HIGH);
+
+  pinMode(PIN_M0,OUTPUT);
+  digitalWrite(PIN_M0,HIGH);
+  pinMode(PIN_M1,OUTPUT);
+  digitalWrite(PIN_M1,HIGH);
+  pinMode(PIN_M2,OUTPUT);
+  digitalWrite(PIN_M2,HIGH);
+
+  init_gy521();
+}
+
+void runMotorLeft(int dir,unsigned long steps)
+{
+  digitalWrite(PIN_DIR_GHE,dir);
+  
+  digitalWrite(PIN_EN_GHE,LOW);
+  delay(10);
+  for (unsigned long i=0;i<steps;i++)
+  {
+    digitalWrite(PIN_STEP_GHE,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(PIN_STEP_GHE,LOW);
+    delayMicroseconds(500);
+  }
+  
+  digitalWrite(PIN_EN_GHE,HIGH);    
+}
+
+void runMotorRight(int dir,unsigned long steps)
+{
+  digitalWrite(PIN_DIR_DTE,dir==HIGH?LOW:HIGH);
+  
+  digitalWrite(PIN_EN_DTE,LOW);
+  delay(10);
+  for (unsigned long i=0;i<steps;i++)
+  {
+    digitalWrite(PIN_STEP_DTE,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(PIN_STEP_DTE,LOW);
+    delayMicroseconds(100);
+  }
+  
+  digitalWrite(PIN_EN_DTE,HIGH);    
+}
+
+void runMotors(int dir,unsigned long steps)
+{
+  digitalWrite(PIN_DIR_GHE,dir);
+  digitalWrite(PIN_DIR_DTE,dir==HIGH?LOW:HIGH);
+  
+  digitalWrite(PIN_EN_GHE,LOW);
+  digitalWrite(PIN_EN_DTE,LOW);
+  delay(10);
+  for (unsigned long i=0;i<steps;i++)
+  {
+    digitalWrite(PIN_STEP_GHE,HIGH);
+    digitalWrite(PIN_STEP_DTE,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(PIN_STEP_GHE,LOW);
+    digitalWrite(PIN_STEP_DTE,LOW);
+    delayMicroseconds(500);
+  }
+  
+  digitalWrite(PIN_EN_GHE,HIGH);    
+  digitalWrite(PIN_EN_DTE,HIGH);    
+}
+
+
+void serialEvent()
+{
+  int cmd=Serial.read();
+  if (cmd=='a')
+  {
+    Serial.println("AV gauche");
+    runMotorLeft(LOW,10000);
+  }
+  else if (cmd=='q')
+  {
+    Serial.println("AR gauche");
+    runMotorLeft(HIGH,10000);
+  }  
+  else if (cmd=='z')
+  {
+    Serial.println("AV droite");
+    runMotorRight(LOW,10000);
+  }
+  else if (cmd=='s')
+  {
+    Serial.println("AR droite");
+    runMotorRight(HIGH,10000);
+  }
+  else if (cmd=='e')
+  {
+    Serial.println("AVANT");
+    runMotors(LOW,2000);
+  }
+  else if (cmd=='d')
+  {
+    Serial.println("ARRIERE");
+    runMotors(HIGH,2000);
+  }
+}
+
+int counter=0;
+
+void loop() 
+{
+  delay(100);
+
+  sensor.read();
+  int ax = sensor.getAccelX();
+  int ay = sensor.getAccelY();
+  int az = sensor.getAccelZ();
+  int gx = sensor.getGyroX();
+  int gy = sensor.getGyroY();
+  int gz = sensor.getGyroZ();
+  int t = sensor.getTemperature();
+
+  if (counter % 10 == 0)
+  {
+    Serial.println("\n\tACCELEROMETER\t\tGYROSCOPE\t\tTEMPERATURE");
+    Serial.println("\tax\tay\taz\tgx\tgy\tgz\tT");
+  }
+
+  Serial.print(counter);
+  Serial.print('\t');
+  Serial.print(ax);
+  Serial.print('\t');
+  Serial.print(ay);
+  Serial.print('\t');
+  Serial.print(az);
+  Serial.print('\t');
+  Serial.print(gx);
+  Serial.print('\t');
+  Serial.print(gy);
+  Serial.print('\t');
+  Serial.print(gz);
+  Serial.print('\t');
+  Serial.print(t);
+  Serial.println();
+
+  counter++;
+}
